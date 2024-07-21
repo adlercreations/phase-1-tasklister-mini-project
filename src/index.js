983eaedf5f3ddd9b6517abea658c48b40dc0427e
Stephen Adler <adlercreations@gmail.com>
2024-07-21 16:20:32 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+//   const taskInput = document.getElementById("taskInput");
+//   const submitButton = document.getElementById("submit");
+//   const taskList = document.getElementById("taskList")
+
+//   const addTodo = () => {
+//     const taskText = taskInput.ariaValueMax. trim();
+//     if (taskText !== "") {
+//       const listItem = document.createElement("li");
+//       listItem.textContent = taskText;
+//       taskList.appendChild(listItem);
+//       taskInput.value = "";
+//     }
+//   }
+//   submitButton.addEventListener("keypress", (event) => {
+//     if (event.key === "Enter") {
+//       addTodo();
+//     }
+//   })
+// });
+    const createTaskForm = document.getElementById(`create-task-form`)
+
+    createTaskForm.addEventListener(`submit`, (event) => {
+      event.preventDefault()
+
+      const taskList = document.getElementById(`tasks`)
+
+      const taskLi = document.createElement(`li`)
+
+      const newTaskDescriptionElement = document.getElementById(`new-task-description`)
+      // console.log(newTaskDescriptionElement)
+      // console.log(newTaskDescriptionElement.value)
+
+      taskLi.append(newTaskDescriptionElement.value + " ")
+
+      const deleteTaskButton = document.createElement(`button`)
+
+      deleteTaskButton.textContent = "X"
+
+      deleteTaskButton.setAttribute(`data-description`, newTaskDescriptionElement.value)
+
+      deleteTaskButton.addEventListener(`click`, () => {
+        // taskLi.remove()
+        taskLi.remove()
+        
+      })
+
+      taskLi.append(deleteTaskButton)
+
+      taskList.append(deleteTaskButton)
+
+      // taskLi.append("hello", document.createElement(`h1`))
+      taskList.appendChild(taskLi)
+
+      createTaskForm.reset()
+    })
+  })
